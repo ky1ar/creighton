af948789e30c4c3bf5549e925fb1ac5f9b0545f8
Kenny Muñoz <kenny@bnka.com>
2024-01-29 12:07:14 -0500

    header

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/css/ky1ar.css?v=2.0.6">
-<link rel="stylesheet" href="assets/css/ky1-res.css?v=1.9">
+<link rel="stylesheet" href="assets/css/ky1ar.css?v=0.0.2">
+<link rel="stylesheet" href="assets/css/ky1-res.css?v=0.0.2">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="assets/js/ky1ar.js?v=2.0.6"></script>
+<script type="text/javascript" src="assets/js/ky1ar.js?v=0.0.2"></script>
