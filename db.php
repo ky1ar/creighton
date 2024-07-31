0b090dd4863e9865db78c33f163c10553d05d7fc
Kenny Muñoz <kenny@munoz.pe>
2024-07-31 18:35:00 -0500

    wip

--- db.php
+++ db.php
@@ -1,10 +1,14 @@
 <?php
-
+$servername = "mysql";
+$username = "root";
+$password = "root";
+$database = "creighton";
+/*
 $servername = "localhost";
 $username = "u196301409_creighton";
 $password = "Knoeht6306*";
 $database = "u196301409_creighton";
-
+*/
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
